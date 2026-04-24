816225d479315e3b479ca75ae898b5f957f108bf
agentswapx <libraboxmaster@hotmail.com>
2026-04-24 15:28:08 +0000

    Update contract addresses in constants for ATX and pool

--- src/constants.ts
+++ src/constants.ts
@@ -16,9 +16,9 @@ export const DEFAULT_RPC_URLS: readonly string[] = [
 export const DEFAULT_RPC_URL = DEFAULT_RPC_URLS[0];
 
 export const DEFAULT_CONTRACTS = {
-  atx: "0xC834d7D575A88c2C81DC1216A693B77aC6483Fca" as Address,
+  atx: "0x82dbfD98AE6741C8506640CE235c6d95570EA638" as Address,
   usdt: "0x55d398326f99059fF775485246999027B3197955" as Address,
-  pool: "0x09aecd2448b99fc913b781cbc660acbab227939b" as Address,
+  pool: "0x157003c4a71697a79f300419eaee271a5f21acaa" as Address,
   swapRouter: "0x13f4EA83D0bd40E75C8222255bc855a974568Dd4" as Address,
   quoter: "0xB048Bbc1Ee6b733FFfCFb9e9CeF7375518e25997" as Address,
   npm: "0x46A15B0b27311cedF172AB29E4f4766fbE7F4364" as Address,
